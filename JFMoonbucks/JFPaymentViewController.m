b2ce63807484a4b56273104713eccf016da06dbb
jonyzfu <dane.radar@gmail.com>
2015-03-31 00:45:44 -0700

    Implemented Payment TableViewCell and UIPickerView

--- JFMoonbucks/JFPaymentViewController.m
+++ JFMoonbucks/JFPaymentViewController.m
@@ -33,7 +33,7 @@
 @property (strong, nonatomic) NSArray *monthArray;
 @property (strong, nonatomic) NSNumber *selectedMonth;
 @property (strong, nonatomic) NSNumber *selectedYear;
-@property (strong, nonatomic) UIPickerView *expirationDataPicker;
+@property (strong, nonatomic) UIPickerView *expirationDatePicker;
 
 @property (strong, nonatomic) AFJSONRequestSerializer *httpSerializer;
 
@@ -41,14 +41,21 @@
 
 @implementation JFPaymentViewController
 
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.monthArray = @[@"01 - January", @"02 - February", @"03 - March",
+                        @"04 - April", @"05 - May", @"06 - June", @"07 - July", @"08 - August", @"09 - September",
+                        @"10 - October", @"11 - November", @"12 - December"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -56,27 +63,185 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Stripe
+
+- (IBAction)completeButtonTapped:(id)sender {
+    //Implement
+}
+
+- (void)validateCustomerInfo {
+    //Implement
+}
+
+- (void)performStripeOperation {
+    //Implement
+}
+
+- (void)postStripeToken:(NSString* )token {
+    //Implement
+}
+
+- (void)handleStripeError:(NSError *) error {
+    //Implement
+}
+
+- (void)chargeDidSucceed {
+    //Implement
+}
+
+- (void)chargeDidNotSuceed {
+    //Implement
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // Return the number of sections.
-    return 0;
+    return 2; // 1, user details; 2, credit card details.
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return (section == 0) ? @"Customer Info" : @"Credit Card Details";
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    // Return the number of rows in the section.
-    return 0;
+    return (section == 0) ? 2 : 3;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
-    // Configure the cell...
+    NSInteger section = indexPath.section;
+    NSInteger row = indexPath.row;
     
-    return cell;
+    if (section == 0 && row == 0) {
+        JFPaymentCardCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"PaymentCardCell"];
+        cell.nameLabel.text = @"Name";
+        cell.textField.placeholder = @"Required";
+        cell.textField.keyboardType = UIKeyboardTypeAlphabet;
+        self.nameTextField = cell.textField;
+        return cell;
+    } else if (section == 0 && row == 1) {
+        JFPaymentCardCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"PaymentCardCell"];
+        cell.nameLabel.text = @"E-mail";
+        cell.textField.placeholder = @"Required";
+        cell.textField.keyboardType = UIKeyboardTypeAlphabet;
+        self.emailTextField = cell.textField;
+        return cell;
+    } else if (section == 1 && row == 0) {
+        JFPaymentCardCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"PaymentCardCell"];
+        cell.nameLabel.text = @"Card Number";
+        cell.textField.placeholder = @"Required";
+        cell.textField.keyboardType = UIKeyboardTypeNumberPad;
+        self.cardNumber = cell.textField;
+        return cell;
+    } else if (section == 1 && row == 1) {
+        JFPaymentCardCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"PaymentCardCell"];
+        cell.nameLabel.text = @"Exp. Date";
+        cell.textField.placeholder = @"Required";
+        cell.textField.textColor = [UIColor lightGrayColor];
+        self.expirationDateTextField = cell.textField;
+        return cell;
+    } else if (section == 1 && row == 2) {
+        JFPaymentCardCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"PaymentCardCell"];
+        cell.nameLabel.text = @"CVC Number";
+        cell.textField.placeholder = @"Required";
+        cell.textField.keyboardType = UIKeyboardTypeNumberPad;
+        self.CVCNumber = cell.textField;
+        [self configurePickerView];
+        return cell;
+    }
+    return nil;
 }
-*/
+
+
+#pragma mark - UITableViewDelegate methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+#pragma mark - UIPicker data source
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 2;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return (component == 0) ? 12 : 10;
+}
+
+#pragma mark - UIPicker delegate
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    if (component == 0) {
+        return self.monthArray[row];
+    } else {
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"yyyy"];
+        NSInteger currentYear = [[dateFormatter stringFromDate:[NSDate date]] integerValue];
+        return [NSString stringWithFormat:@"%li", currentYear + row];
+    }
+    return nil;
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    if (component == 0) {
+        self.selectedMonth = @(row + 1);
+    } else {
+        NSString *yearString = [self pickerView:self.expirationDatePicker titleForRow:row forComponent:1];
+        self.selectedYear = @([yearString integerValue]);
+    }
+    
+    if (!self.selectedMonth) {
+        [self.expirationDatePicker selectRow:0 inComponent:0 animated:YES];
+        self.selectedMonth = @(1); // Default to January
+    }
+    
+    if (!self.selectedYear) {
+        [self.expirationDatePicker selectRow:0 inComponent:0 animated:YES];
+        NSString *yearString = [self pickerView:self.expirationDatePicker titleForRow:0 forComponent:1];
+        self.selectedYear = @([yearString integerValue]); // Default to current year
+    }
+    
+    self.expirationDateTextField.text = [NSString stringWithFormat:@"%@/%@", self.selectedMonth, self.selectedYear];
+    self.expirationDateTextField.textColor = [UIColor blackColor];
+}
+
+
+#pragma mark - UIPicker configuration
+
+- (void)configurePickerView {
+    self.expirationDatePicker = [[UIPickerView alloc] init];
+    self.expirationDatePicker.delegate = self;
+    self.expirationDatePicker.dataSource = self;
+    self.expirationDatePicker.showsSelectionIndicator = YES;
+    
+    // Create and configure toolbar that holds "Done button"
+    UIToolbar *pickerToolbar = [[UIToolbar alloc] init];
+    pickerToolbar.barStyle = UIBarStyleBlackTranslucent;
+    [pickerToolbar sizeToFit];
+    
+    UIBarButtonItem *flexibleSpaceLeft = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                                                       target:nil
+                                                                                       action:nil];
+    
+    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Done"
+                                                                   style:UIBarButtonItemStyleDone
+                                                                  target:self
+                                                                  action:@selector(pickerDoneButtonPressed)];
+    [pickerToolbar setItems:[NSArray arrayWithObjects:flexibleSpaceLeft, doneButton, nil]];
+    
+    self.expirationDateTextField.inputView = self.expirationDatePicker;
+    self.expirationDateTextField.inputAccessoryView = pickerToolbar;
+    self.nameTextField.inputAccessoryView = pickerToolbar;
+    self.emailTextField.inputAccessoryView = pickerToolbar;
+    self.cardNumber.inputAccessoryView = pickerToolbar;
+    self.CVCNumber.inputAccessoryView = pickerToolbar;
+}
+
+- (void)pickerDoneButtonPressed {
+    [self.view endEditing:YES];
+}
 
 /*
 // Override to support conditional editing of the table view.
